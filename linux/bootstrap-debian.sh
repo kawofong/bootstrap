a40ebf172acf2650aa07f01d09d14f59c31f19d7
kawo123 <14829553+kawo123@users.noreply.github.com>
2021-12-10 04:15:13 +0000

    Add GCP SDK and Terraform to debian bootstrap

--- linux/bootstrap-debian.sh
+++ linux/bootstrap-debian.sh
@@ -36,8 +36,15 @@ source ./logging.sh
 ##############################################################################
 
 PACKAGES=(
+    apt-transport-https
+    ca-certificates
+    software-properties-common
+    curl
     git-all
+    gnupg
     jq
+    kubectl
+    lsb-release
 )
 
 
@@ -56,12 +63,6 @@ install_docker() {
     # Reference: https://docs.docker.com/engine/install/debian/
     sudo apt-get remove docker docker.io containerd runc | true # ignore if don't exist
     info "Removed old versions of docker."
-    sudo apt-get update
-    sudo apt-get install \
-        ca-certificates \
-        curl \
-        gnupg \
-        lsb-release
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     info "Added Docker’s official GPG key."
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
@@ -69,15 +70,33 @@ install_docker() {
     info "Set up the docker stable repository."
     sudo apt update
     sudo apt-get install docker-ce docker-ce-cli containerd.io
-    info "Docker installation complete."
+    info "Docker installation completes."
+}
+
+install_google_cloud_sdk() {
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    info "Added Google Cloud SDK distribution URI as a package source."
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+    info "Imported the Google Cloud public key."
+    sudo apt-get update && sudo apt-get install google-cloud-sdk
+    info "Google Cloud SDK installation completes."
+}
+
+install_terraform() {
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+    info "Added the HashiCorp GPG key."
+    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+    info "Added the official HashiCorp Linux repository."
+    sudo apt-get update && sudo apt-get install terraform
+    info "Terraform installation completes."
 }
 
 generate_git_ssh_key() {
     if [[ ! -f ~/.ssh/id_rsa ]]; then
         ssh-keygen -t rsa -C "14829553+kawo123@users.noreply.github.com"
-        info '##### Please see below for SSH public key: '
+        info "##### Please see below for SSH public key: "
         cat ~/.ssh/id_rsa.pub
-        info '##### Follow step 4 to complete: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account'
+        info "##### Follow step 4 to complete: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account"
         info '##### After you added SSH key to your GitHub account, you can run "ssh -T git@github.com" to verify your configuration.'
     fi
 }
@@ -85,6 +104,8 @@ generate_git_ssh_key() {
 ### Runtime
 ##############################################################################
 
-# install_apt_packages
-install_docker
+install_apt_packages
+# install_docker
+install_google_cloud_sdk
+# install_terraform
 generate_git_ssh_key
